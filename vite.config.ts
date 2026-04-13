24313cd816325751436b0f44fc170421a437c9aa
CareZapato <pbarrerap@1988gmail.com>
2026-04-13 19:05:52 -0400

    deploy render

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,10 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ['d4build.onrender.com']
   }
 })
